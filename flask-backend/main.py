33b7f91fb7dc99401c59fcde4b72be1153c6b33f
meoinghbh <moein.ghobbeh@gmail.com>
2019-10-30 08:55:05 +0330

    add

---
+++ flask-backend/main.py
@@ -0,0 +1,14 @@
+import flask
+
+app = flask.Flask("__main__")
+@app.route("/")
+def my_index():
+    return flask.render_template("index.html",token="Hello Flask")
+app.run(debug=True)
+
+
+
+
+
+
+
---
+++ flask-backend/static/react/precache-manifest.2c1ce8337ceb991cc4e3f3d908ddcc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d4930b54c07ceca1f06be0c67023e7",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "b57b360b5c4cb1e2a405",
+    "url": "/static/react/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "92b1125301498f0f2b59",
+    "url": "/static/react/js/2.084cf5e0.chunk.js"
+  },
+  {
+    "revision": "b57b360b5c4cb1e2a405",
+    "url": "/static/react/js/main.d1b00add.chunk.js"
+  },
+  {
+    "revision": "4a5017080316628fd930",
+    "url": "/static/react/js/runtime-main.24f57757.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/react/media/logo.25bf045c.svg"
+  }
+]);

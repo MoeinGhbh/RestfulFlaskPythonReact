8e8bc75b26a15a80b81fcb18fb071dc8e283fc2b
Moeinghbh <moein.ghobbeh@gmail.com>
2019-12-15 10:56:37 +0330

    table

--- react-frontend/src/ui/templates/ChangePassword.jsx
+++ react-frontend/src/ui/templates/ChangePassword.jsx
@@ -5,7 +5,32 @@ class ChangePassword extends Component {
     render(){
         return(
             <div>
-
+                <table>
+                    <tr>
+                        <td>
+                            <label>رمز قبلی</label>
+                        </td>
+                        <td>
+                            <inpu></inpu>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>رمز جدید</label>
+                        </td>
+                        <td>
+                            <input/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>تکرار رمز جدید</label>
+                        </td>
+                         <td>
+                            <input/>
+                        </td>
+                    </tr>
+                </table>
             </div>
         )
     }

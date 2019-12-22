36ee31a58d03dedd9798dec3cece0f842abb04bd
meoinghbh <moein.ghobbeh@gmail.com>
2019-12-22 11:23:07 +0330

    edit

--- react-frontend/src/ui/templates/Home.jsx
+++ react-frontend/src/ui/templates/Home.jsx
@@ -8,51 +8,66 @@ import img from "../img/BMS.jpg";
 
 class Home extends Component {
     constructor(props) {
-    super(props);
-    this.state = {
-            data: []}
-    this.handler = this.handler.bind(this);
-    const myRole = localStorage.getItem("LSrole")
-    const myToken =   localStorage.getItem("LStoken")
-  }
-   componentDidMount() {
-      axios.post('http://127.0.0.1:5000/api/v1.0/perRoleHome?token='+ localStorage.getItem("LStoken"), {"role":localStorage.getItem("LSrole")})
-            .then(res =>{
-                this.setState({data: res.data.data})
-            })
-       setInterval(()=> axios.post('http://127.0.0.1:5000/api/v1.0/perRoleHome?token='+ localStorage.getItem("LStoken"), {"role":localStorage.getItem("LSrole")})
-            .then(res =>{
-                this.setState({data: res.data.data})
-            }),2000)
+        super(props);
+        this.state = {
+            data: []
+        }
+        this.handler = this.handler.bind(this);
+        const myRole = localStorage.getItem("LSrole")
+        const myToken = localStorage.getItem("LStoken")
     }
 
-  handler=(data,zoneIndex, name, zoneId)=> {
-      //console.log("data" ,data,zoneIndex,name, zoneId);
-      axios.post("http://127.0.0.1:5000/api/v1.0/update?token="+ localStorage.getItem("LStoken"),{"zoneIndex":zoneIndex,"dataitem":data,"nameZone":name, "zoneId":zoneId })
-          .then(res=>{
-              if(res.status==200){
-                  this.state.data[parseInt(zoneIndex)].items=data;
-                  this.setState({...this.state});
-          }
-      })
-  }
+    componentDidMount() {
+        axios.post('http://127.0.0.1:5000/api/v1.0/perRoleHome?token=' + localStorage.getItem("LStoken"), {"role": localStorage.getItem("LSrole")})
+            .then(res => {
+                if (res.status == 200) {
+                    this.setState({data: res.data.data})
+                }
+            })
+        setInterval(() => axios.post('http://127.0.0.1:5000/api/v1.0/perRoleHome?token=' + localStorage.getItem("LStoken"), {"role": localStorage.getItem("LSrole")})
+            .then(res => {
+                if (res.status == 200) {
+                    this.setState({data: res.data.data})
+                }
+            }).catch(r => {
+                this.props.history.push("/")
+                } )
 
-  render() {
-    const { data, handler,history } = this.props;
-    console.log(history.location.state)
-    return (
-      <div class="container-fluid">
-        <h1>BMS</h1>
-        <p> Building Management System is way to make smart homes</p>
-        <Navigator data={this.state.data} handler={this.handler}  />
-        <br />
-        {/* <div className="icone" id="icone">
+            , 2000)
+    }
+
+    handler = (data, zoneIndex, name, zoneId) => {
+        //console.log("data" ,data,zoneIndex,name, zoneId);
+        axios.post("http://127.0.0.1:5000/api/v1.0/update?token=" + localStorage.getItem("LStoken"), {
+            "zoneIndex": zoneIndex,
+            "dataitem": data,
+            "nameZone": name,
+            "zoneId": zoneId
+        })
+            .then(res => {
+                if (res.status == 200) {
+                    this.state.data[parseInt(zoneIndex)].items = data;
+                    this.setState({...this.state});
+                }
+            })
+    }
+
+    render() {
+        const {data, handler, history} = this.props;
+        console.log(history.location.state)
+        return (
+            <div class="container-fluid">
+                <h1>BMS</h1>
+                <p> Building Management System is way to make smart homes</p>
+                <Navigator data={this.state.data} handler={this.handler}/>
+                <br/>
+                {/* <div className="icone" id="icone">
           <img src={img} alt="" className="img" />
         </div>  */}
-        <ZoneSection data={this.state.data} handler={this.handler} />
-      </div>
-    );
-  }
+                <ZoneSection data={this.state.data} handler={this.handler}/>
+            </div>
+        );
+    }
 }
 
 export default Home;

0506e4e9721a02769de2defdc2d9a51e7355c8da
meoinghbh <moein.ghobbeh@gmail.com>
2019-12-23 17:10:18 +0330

    add push state to page by link

--- react-frontend/src/ui/components/Navigator/Navigation.jsx
+++ react-frontend/src/ui/components/Navigator/Navigation.jsx
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MenuItem from "./MenuItems";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import {NavLink} from "react-router-dom";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import FormControl from "react-bootstrap/FormControl";
 
@@ -13,7 +14,7 @@ export default class NavagationSection extends Component {
 
     render() {
         const {data} = this.props;
-        console.log(data)
+        console.log("navigation ", data);
         return (
             <div>
                 <session>
@@ -23,60 +24,21 @@ export default class NavagationSection extends Component {
                                 {data.map(submenu => {
                                     return <MenuItem zoneName={submenu.zoneName}/>;
                                 })}
-                                {/* <NavDropdown.Item href="#TVRome/3.1">
-                  Intro
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                TV Rome
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Kitchen
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Bed Rome Master
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.4">
-                  Bed Rome North
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.4">
-                  Bed Rome South
-                </NavDropdown.Item> */}
+
                             </NavDropdown>
-                            <Nav.Link href="/Panel" to={{
-                                      pathname: '/Panel',
-                                      state: {
-                                        state: {data}
-                                      }
-                                    }}>Control Panel</Nav.Link>
+
+                            <NavLink className={"nav-link"} href="/Panel"
+                                     to={{
+
+                                         pathname: '/Panel',
+                                         state: data
+                                     }}>Control Panel</NavLink>
                             <Nav.Link href="/ChangePassword">Change Password</Nav.Link>
                         </Nav>
-                        {/* <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-              <Button variant="outline-light">Search</Button>
-            </Form> */}
+
                         <Navbar.Brand href="#home">به خانه ی هوشمند خوش آمدید</Navbar.Brand>
                     </Navbar>
-                    {/* <Nav justify variant="tabs" defaultActiveKey="/home">
-            <Nav.Item>
-              <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="Login">Login</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="ChangePassword">ChangePassword</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="disabled" disabled>
-                Disabled
-              </Nav.Link>
-            </Nav.Item>
-          </Nav> */}
+
                 </session>
             </div>
         );

f8550d1da10a633b5a133d49481b89cbe7af9ccd
meoinghbh <moein.ghobbeh@gmail.com>
2019-12-25 14:28:10 +0330

    add new

--- react-frontend/src/ui/components/CreatePart.jsx
+++ react-frontend/src/ui/components/CreatePart.jsx
@@ -6,9 +6,59 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import {distancArray} from "../../Helper";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 class CreatePart extends Component {
+    constructor() {
+        super()
+        this.state = {
+            checkState: false,
+            newZone: '',
+            newZonerole: '',
+            roles: []
+        }
+    }
+
+    componentDidMount() {
+        axios.post('http://127.0.0.1:5000/api/v1.0/GetAllroles?token=' + localStorage.getItem('LStoken'))
+            .then(res => {
+                this.setState({roles: res.data.data})
+            }).catch(res => {
+            this.props.history.push("/")
+        })
+    }
+
+    updateState = (e) => {
+        console.log(e);
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    addZone = (e) => {
+        //console.log(localStorage.getItem("LStoken"))
+        const {newZone, newZonerole} = this.state
+        axios.post("http://127.0.0.1:5000/api/v1.0/addZone?token=" + localStorage.getItem("LStoken"),
+            {
+                "newZone": newZone,
+                "role": newZonerole
+            })
+            .then(res => {
+                    this.setState({roles: res});
+                }
+            )
+            .catch(() =>
+                // r => alert(r)
+                alert("این قسمت قبلا اضافه شده است")
+            )
+    }
+
+    changeCheckState = (e) => {
+        console.log("e=>>> ", e);
+        this.setState({checkState: e.target.checked})
+    }
+
+
     render() {
+        const {roles} = this.state;
         return (
             <div>
                 <table>
@@ -25,7 +75,8 @@ class CreatePart extends Component {
                         <td>
                             <Switch
                                 checked={this.state.checkState}
-                                onChange={this.updateState}
+                                onChange={this.changeCheckState}
+                                name={"checkState"}
                             />
                         </td>
                         {this.state.checkState == true ?
@@ -34,7 +85,7 @@ class CreatePart extends Component {
                                 <input type="text"
                                        name="newZone"
                                        value={this.state.newZone}
-                                       onChange={this.getZoneRole}/>
+                                       onChange={this.updateState}/>
                                 نقش را انتخاب نمایید
 
                                 <FormControl>
@@ -43,23 +94,18 @@ class CreatePart extends Component {
                                         labelId="lblRole"
                                         id="demo-simple-selectaaaa"
                                         value={this.state.newZonerole}
-                                        onChange={this.getZoneRole}
+                                        onChange={this.updateState}
                                         name="newZonerole"
                                     >
-                                        {/*<input type="text"*/}
-                                        {/*       value={this.state.newZonerole}*/}
-                                        {/*       onChange={this.getZoneRole}*/}
-                                        {/*       name="newZonerole"/>*/}
-
-                                        <MenuItem value="">
+                                        <MenuItem value={"none"}>
                                             <em>None</em>
                                         </MenuItem>
-                                        {/*{*/}
-                                        {/*    distancArray(data, "accessLevel").map(roles => {*/}
-                                        {/*        return <MenuItem*/}
-                                        {/*            value={roles}>{roles}</MenuItem>*/}
-                                        {/*    })*/}
-                                        {/*}*/}
+                                        {
+                                            roles.map(roles => {
+                                                return (<MenuItem
+                                                    value={roles.id}>{roles.role}</MenuItem>)
+                                            })
+                                        }
                                     </Select>
                                 </FormControl>
 

0880862402f2686b9982fc660a36ce4c3ac200d8
meoinghbh <moein.ghobbeh@gmail.com>
2020-01-04 07:42:39 +0330

    add new

--- react-frontend/src/ui/components/Zones/ZoneSetion.js
+++ react-frontend/src/ui/components/Zones/ZoneSetion.js
@@ -1,29 +1,25 @@
 import React, {Component} from "react";
 import "./css/ZoneSection.css";
 import ZoneCard from "./ZoneCard.js";
-import {forEach} from "react-bootstrap/cjs/utils/ElementChildren";
-//import data from "../../../Data";
 
 export default class ZoneSection extends Component {
     render() {
         const {data, handler} = this.props;
         return (
-
-
             <div id="Zones">
                 {
                     data.map((Zone, index) => {
                         return (<div id="Zone">
-                                <a id={Zone.zoneName}>
-                                         <h2> {Zone.zoneName} </h2>
-                                                 <ZoneCard
-                                                    zoneIndex={index}
-                                                    zoneName={Zone.zoneName}
-                                                    items={Zone.items}
-                                                    handler={handler}
-                                                    zoneId={Zone.zoneId}
-                                                    className="card"/>
-                                </a>
+                            <a id={Zone.zoneName}>
+                                <h2> {Zone.zoneName} </h2>
+                                <ZoneCard
+                                    zoneIndex={index}
+                                    zoneName={Zone.zoneName}
+                                    items={Zone.items}
+                                    handler={handler}
+                                    zoneId={Zone.zoneId}
+                                    className="card"/>
+                            </a>
                         </div>)
                     })
                 }

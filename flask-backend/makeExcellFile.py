bc24c48509fb7b7efa6296f0da6aa84c74d24e85
meoinghbh <moein.ghobbeh@gmail.com>
2020-01-04 20:59:14 +0330

    add new

--- flask-backend/makeExcellFile.py
+++ flask-backend/makeExcellFile.py
@@ -17,8 +17,6 @@ class makeExcellFile():
         wb = openpyxl.load_workbook('List.xlsx')
         sheet = wb['List']
 
-
-
         for row in sheet.iter_rows():
             for cell in row:
                 if cell.value == None:
@@ -37,7 +35,7 @@ class makeExcellFile():
 
                         if ((item['group'] == 'Aircondition') and (item['speedType'] == 'Slow') or (
                                 item['group'] != 'Aircondition')):
-                            sheet.cell(row=cell.row, column=1).value = cell.row+17
+                            sheet.cell(row=cell.row, column=1).value = cell.row + 17
                             sheet.cell(row=cell.row, column=4).value = item['itemId']
                             sheet.cell(row=cell.row, column=5).value = item['itemName']
                             sheet.cell(row=cell.row, column=6).value = item['group']

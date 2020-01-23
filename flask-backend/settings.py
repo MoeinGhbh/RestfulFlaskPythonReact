ddd4f61cb59a3c1bb6c1e01df1637120229644f9
meoinghbh <moein.ghobbeh@gmail.com>
2020-01-23 16:20:53 +0100

    edit address for database path from static to dynamic

--- flask-backend/settings.py
+++ flask-backend/settings.py
@@ -6,5 +6,3 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users//Rose//Desktop//SmartHome//flask-backend//database.db "
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # app.config["SERVER_NAME"] = "localhost:8888"
-
-
